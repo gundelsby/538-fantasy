ae30980c1969996174e61252dca97fc6f62aa146
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2018-04-30 17:52:20 +0200

    Added code for parsing teams and matches

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+import teamParse from './data/team.js'
+
+// window.fetch('https://projects.fivethirtyeight.com/soccer-predictions/forecasts/2018_eliteserien_matches.json')
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(json => {
+//     console.log(json)
+//   })
+
+window.fetch('https://projects.fivethirtyeight.com/soccer-predictions/forecasts/2018_eliteserien_forecast.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(json => {
+    json.forecasts[0].teams.forEach((team) => {
+      console.log(teamParse(team))
+    })
+  })
