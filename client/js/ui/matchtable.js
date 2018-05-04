76759b76e40c9db602fde7d719bc9b3e9c90f19d
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2018-05-04 17:25:21 +0200

    Color coded probability for matches, with home/away marking

--- client/js/ui/matchtable.js
+++ client/js/ui/matchtable.js
@@ -2,18 +2,29 @@ const MATCHTABLE_ID = 'matchtable'
 
 const teamCache = {}
 
+const difficulty = [
+  {name: 'baby', class: 'difficulty--baby'},
+  {name: 'easy', class: 'difficulty--easy'},
+  {name: 'medium', class: 'difficulty--medium'},
+  {name: 'hard', class: 'difficulty--hard'},
+  {name: 'nightmare', class: 'difficulty--nightmare'}
+]
+
 function fillTeamCache (teams) {
   teams.forEach(team => {
     teamCache[team.id] = team
   })
 }
 
-function calcBackgroundColor (match, isHomeGame) {
-  const r = Math.round(((isHomeGame ? match.prob2 : match.prob1) * 100))
-  const g = Math.round(((isHomeGame ? match.prob1 : match.prob2) * 100))
-  const b = 0
-
-  return `rgba(${r}, ${g}, ${b}, 0.5)`
+function calcDifficultyClass (match, isHomeGame) {
+  const adversityFactor = 1.0 - ((isHomeGame ? match.prob1 : match.prob2) + match.probtie / 2)
+  const difficultyPosition = Math.ceil(adversityFactor * difficulty.length)
+  const calculatedDifficulty = difficulty[difficultyPosition - 1]
+  if (match.id === 401007367) {
+    console.log(isHomeGame ? match.prob1 : match.prob2, match.probtie, isHomeGame ? match.prob2 : match.prob1)
+    console.log(adversityFactor, difficultyPosition, calculatedDifficulty && calculatedDifficulty.name)
+  }
+  return calculatedDifficulty ? calculatedDifficulty.class : ''
 }
 
 function createMatchCell (currentTeamId, match) {
@@ -21,8 +32,8 @@ function createMatchCell (currentTeamId, match) {
   const isHomeGame = match.team1_id === currentTeamId
   const opponent = isHomeGame ? teamCache[match.team2_id] : teamCache[match.team1_id]
 
-  td.textContent = opponent.code
-  td.style.backgroundColor = calcBackgroundColor(match, isHomeGame)
+  td.textContent = `${opponent.code} (${isHomeGame ? 'H' : 'B'})`
+  td.className = calcDifficultyClass(match, isHomeGame)
 
   return td
 }
